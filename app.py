182413af86abfe280b51858ddaffcf00f3b0620a
Ivan02alvaro <charca.mamani.alvaro@itdonbosco.org>
2025-04-28 13:36:35 -0400

    se agrego el repositorio

--- app.py
+++ app.py
@@ -9,10 +9,16 @@ def home():
 @app.route('/about')
 def about():
     return "<h1><strong>Esta es la página de About.</strong></h1><p>Esta es una página de ejemplo para mostrar cómo funciona Flask.</p>"
+#renderizar paginas para reutilizar codigo
+#flask permite mandar es decir soporta listas y diccionarios
+#{{ t }} permite iterar (esta en tasks) la t puede ser otra variable mas comoda pero tambien afectaria al for 
+#creado en tasks
+
+
 
 @app.route('/tasks')
 def list_tasks():
-    tarea = "Lavar la ropa"
+    tarea = ["Lavar la ropa","Limpiar la casa","Hacer la compra", "Estudiar para el examen","Hacer ejercicio","Leer un libro"]
     return render_template('tasks.html', t=tarea)
 
 if __name__ == '__main__':
